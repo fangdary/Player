176d7a31e3a3a6deca167886409516d55209f4ff
EG <EG@EGMac.local>
2017-04-12 14:11:59 +0800

    Initial

---
+++ Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Player
+//
+//  Created by EG on 2017/4/12.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
